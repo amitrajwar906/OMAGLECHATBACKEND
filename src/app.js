d8502288684cebce696b5316eb67a06cd27efc2f
Amit Rajwar <amitxrajwar@gmail.com>
2026-02-14 22:21:40 -0800

    first commit

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 
+app.set('trust proxy', 1);
+
 // CORS configuration
 const corsOptions = {
   origin: function (origin, callback) {
